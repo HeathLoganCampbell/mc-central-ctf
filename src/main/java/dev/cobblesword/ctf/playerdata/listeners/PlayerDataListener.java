4cd294c2044e4d9adc01e85630d2abba756dde5c
Heath L Campbell <GitHub@HeathLoganCampbell.com>
2023-09-23 22:00:07 +1200

    add event handler

--- src/main/java/dev/cobblesword/ctf/playerdata/listeners/PlayerDataListener.java
+++ src/main/java/dev/cobblesword/ctf/playerdata/listeners/PlayerDataListener.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -23,6 +24,12 @@ public class PlayerDataListener implements Listener
     public void onLogin(AsyncPlayerPreLoginEvent e)
     {
         this.playerDataManager.onFetch(e.getUniqueId());
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e)
+    {
+        this.playerDataManager.onJoin(e.getPlayer().getUniqueId());
     }
 
     @EventHandler
